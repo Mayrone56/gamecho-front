a190f7cbdf059ade4b691de007ad0f7b086474c9
Valentin <sighwayve@gmail.com>
2024-05-20 15:10:22 +0200

    redux store configuration

---
+++ pages/_app.js
@@ -0,0 +1,24 @@
+import '../styles/globals.css';
+import Head from 'next/head';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import user from '../reducers/user'
+
+const store = configureStore({
+  reducer: { user },
+ });
+
+function App({ Component, pageProps }) {
+  return (
+    <>
+     <Provider store={store}>
+      <Head>
+        <title>Next.js App</title>
+      </Head>
+      <Component {...pageProps} />
+      </Provider>
+    </>
+  );
+}
+
+export default App;
