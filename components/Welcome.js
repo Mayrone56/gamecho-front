7badeb4d4ade88afcf0e7a85b98e7b35edbae252
Paul Thibault <paulthibault75014@gmail.com>
2024-05-21 09:26:51 +0200

    critickerlike

--- components/Welcome.js
+++ components/Welcome.js
@@ -10,7 +10,7 @@ function Welcome() {
     }
     
     return (
-        <>
+        <>  
             <div className={styles.main} >
                 <div className={styles.container} >
                     <div className={styles.title} >
