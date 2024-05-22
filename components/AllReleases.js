3b60e766a8de62b87c85e3cd125820099be11917
Gtfoohfs <karl.hosni@icloud.com>
2024-05-22 14:58:57 +0200

    ? wishlist

--- components/AllReleases.js
+++ components/AllReleases.js
@@ -3,36 +3,38 @@ import Header from './Header';
 import Footer from './Footer';
 import { useEffect, useState } from 'react';
 
-const [games, setGames] = useState([])
-
-// useEffect pour éviter un infinite re-render (préconisation React)
-// exploitation des données reçues de l'API: data.results >>name & background_image
-// On map sur le tableau results pour retourner les données souhaitées.
-useEffect(() => {
-    fetch('http://localhost:3000/games')
-        .then(response => response.json())
-        .then(data => {
-            const formatedData = data.results.map(games => {
-                const card = `https://media.rawg.io/media/games/${games.background_image}`;
-                let gameName = games.name;
-
-                return { title: gameName, card };
-
-            });
-            setGames(formatedData);
-        });
-
-});
-
 
 
 function AllReleases() {
+
+    const [games, setGames] = useState([])
+
+    // useEffect pour éviter un infinite re-render (préconisation React)
+    // exploitation des données reçues de l'API: data.results >>name & background_image
+    // On map sur le tableau results pour retourner les données souhaitées.
+
+    useEffect(() => {
+        fetch('http://localhost:3000/games')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                const formatedData = data.games.map(games => {
+                    const card = `https://media.rawg.io/media/games/${games.background_image}`;
+                    let gameName = games.name;
+
+                    return { title: gameName, card };
+
+                });
+                setGames(formatedData);
+            });
+    }, []);
+
+
     return (
         <div className={styles.container}>
             <Header />
             <div className={styles.middleContainer}>
                 <h2 className={styles.sectionTitle}>All releases</h2>
-                <p>{games}</p>
             </div>
             <Footer />
         </div>
