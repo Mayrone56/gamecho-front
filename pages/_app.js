337f8380ecd32d8dbc8e577271e2e0959c42a247
Paul Thibault <paulthibault75014@gmail.com>
2024-05-23 16:47:38 +0200

    reducer

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,10 @@ import Head from 'next/head';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import user from '../reducers/user'
+import config from '../reducers/config';
 
 const store = configureStore({
-  reducer: { user },
+  reducer: { user,config},
  });
 
 function App({ Component, pageProps }) {
