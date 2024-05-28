8c3812c99d24c49297e76f334b1a0697fdf9fdbd
Paul Thibault <paulthibault75014@gmail.com>
2024-05-28 15:17:58 +0200

    sqdqsd

--- components/GameCard.js
+++ components/GameCard.js
@@ -4,16 +4,7 @@ import { useState } from "react";
 
 
 function GameCard({ game, isAddedToWishlist, onHeartClick, onClick }) { // les deux dernière props exploitent l'inverse data flow ! la fonction est initiée dans Home, recupérée dans la cart qui renvoie les informations dans home selon la prop classique game !!!
-  let iconStyle = {};
-  //on declare un objet vide qui contiendra les propriétés de style CSS pour l'icône
-  //on vérifie si la variable isAddedToWishlist est vraie (si le jeu en cours est dans la liste de souhaits ou non)
-  if (isAddedToWishlist) {
-    //si c'est vrai, on change la couleur de l'icône( on utilise le filtre css car l'icône n'est pas remplie, elle a un centre transparent et on ne change que la couleur de ses bordures - pour expliquer le filtre en détail, demandez à chatGPT de vous donner un exemple), vous pouvez utiliser https://isotropic.co/tool/hex-color-to-css-filter/ pour faire une conversion de couleur depuis un #hexadecimal
-    iconStyle = {
-      filter:
-        "invert(47%) sepia(89%) saturate(7473%) hue-rotate(1deg) brightness(102%) contrast(105%)",
-    };
-  }
+
 
 
   return (
@@ -26,7 +17,7 @@ function GameCard({ game, isAddedToWishlist, onHeartClick, onClick }) { // les d
     >
       <p className={styles.gameNameCard}>{game.name}</p>
       <button
-        className={styles.iconButton}
+        className={isAddedToWishlist?styles.iconButtonPink:styles.iconButton}
         onClick={onHeartClick}
         aria-label={
           isAddedToWishlist ? "Remove from wishlist" : "Add to wishlist"
@@ -38,7 +29,6 @@ function GameCard({ game, isAddedToWishlist, onHeartClick, onClick }) { // les d
           width={24}
           height={24}
           className={styles.likeIcon}
-          style={iconStyle}
         />
       </button>
     </div>
