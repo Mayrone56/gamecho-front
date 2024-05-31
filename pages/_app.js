1e63ffb49107137839b6ec528045d9bef8e1c4fc
Valentin <sighwayve@gmail.com>
2024-05-31 14:48:33 +0200

    Persistor added and updated delete handle in Rating s component

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,38 @@
-import '../styles/globals.css';
-import Head from 'next/head';
-import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
-import user from '../reducers/user'
-import config from '../reducers/config';
-import wishlist from '../reducers/wishlist';
-import game from '../reducers/game';
-import rating from '../reducers/rating';
+import "../styles/globals.css";
+import Head from "next/head";
+import { Provider } from "react-redux";
+import user from "../reducers/user";
+import config from "../reducers/config";
+import wishlist from "../reducers/wishlist";
+import game from "../reducers/game";
+import rating from "../reducers/rating";
+import { persistStore, persistReducer } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import storage from "redux-persist/lib/storage";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+
+const reducers = combineReducers({ config, game, rating, user, wishlist });
+
+const persistConfig = { key: "gamecho", storage };
 
 const store = configureStore({
-  reducer: { user, config, wishlist, game, rating},
- });
+  reducer: persistReducer(persistConfig, reducers),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
+});
 
+const persistor = persistStore(store);
 
 function App({ Component, pageProps }) {
   return (
     <>
       <Provider store={store}>
-        <Head>
-          <title>Next.js App</title>
-        </Head>
-        <Component {...pageProps} />
+        <PersistGate persistor={persistor}>
+          <Head>
+            <title>Next.js App</title>
+          </Head>
+          <Component {...pageProps} />
+        </PersistGate>
       </Provider>
     </>
   );
